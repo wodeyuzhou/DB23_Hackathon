d1c4834dc00205e6690a35e37f8b55c7960a7e46
Janghwan <kimjanghwan1@gmail.com>
2024-01-11 11:11:15 +0900

    Update README and fixed code

--- README.md
+++ README.md
@@ -1,1 +1,28 @@
-# DB23_Hackathon
+# DB23 Hackathon-bike-sharing-demand
+https://www.kaggle.com/competitions/db23-hackathon/overview
+- code/ - main code
+    - main.py - solution based on Gradient Boost and Random Forest, rank 1 (55.63506) within 60 lines of code
+    - main.ipynb - detailed solution with some explanations
+- data/ - the datasets
+- output/ - output of the solution
+
+### Data Fields
+- dteday : date
+- season : season (1:winter, 2:spring, 3:summer, 4:fall)
+- holiday : weather day is holiday or not (extracted from http://dchr.dc.gov/page/holiday-schedule)
+- weekday : day of the week
+- workingday : if day is neither weekend nor holiday is 1, otherwise is 0.
+- weathersit :
+    1: Clear, Few clouds, Partly cloudy, Partly cloudy
+    2: Mist + Cloudy, Mist + Broken clouds, Mist + Few clouds, Mist
+    3: Light Snow, Light Rain + Thunderstorm + Scattered clouds, Light Rain + Scattered clouds
+    4: Heavy Rain + Ice Pallets + Thunderstorm + Mist, Snow + Fog
+- temp : Normalized temperature in Celsius. The values are derived via (t-t_min)/(t_max-t_min), t_min=-8, t_max=+39 (only in hourly scale)
+- atemp: Normalized feeling temperature in Celsius. The values are derived via (t-t_min)/(t_max-t_min), t_min=-16, t_max=+50 (only in hourly scale)
+- hum: Normalized humidity. The values are divided to 100 (max)
+- windspeed: Normalized wind speed. The values are divided to 67 (max)
+- cnt: count of total rental bikes including both casual and registered
+
+### Cited
+https://github.com/logicalguess/kaggle-bike-sharing-demand
+https://github.com/qinhanmin2014/kaggle-bike-sharing-demand
---
+++ code/.ipynb_checkpoints/main-checkpoint.py
@@ -0,0 +1,53 @@
+import numpy as np
+import pandas as pd
+from sklearn.ensemble import RandomForestRegressor, GradientBoostingRegressor
+from sklearn.metrics import mean_squared_error
+
+train = pd.read_csv("data/train.csv", index_col=0)
+test = pd.read_csv("data/test.csv", index_col=0)
+
+data = pd.concat([train, test], axis=0)
+data.index = pd.to_datetime(data.index)
+
+data['year'] = data.index.year
+data['month'] = data.index.month
+data['day'] = data.index.day
+data['weekday'] = data.index.weekday
+data['dayofweek'] = data.index.dayofweek 
+data['hour'] = data.index.hour
+data['year_season'] = data['year'] + data['season'] / 10
+
+test_start = '2012-12-01'
+train_data = data[data.index < test_start].copy()
+test_data = data[data.index >= test_start].copy()
+
+train_data['cnt_log'] = np.log(train_data['cnt'] + 1)
+
+by_season = train_data.groupby('year_season')['cnt'].median()
+by_season = by_season.rename('count_season')
+
+train_data = train_data.join(by_season, on='year_season')
+test_data = test_data.join(by_season, on='year_season')
+
+regs = {"gbdt": GradientBoostingRegressor(random_state=0),
+        "rf": RandomForestRegressor(random_state=0, n_jobs=-1)}
+
+features = ['season', 'holiday', 'workingday', 'weathersit', 'temp', 'atemp', 'hum', 'windspeed', 'year', 'hour', 'dayofweek', 'count_season']
+
+preds = {}
+for name, reg in regs.items():
+    if name == 'gbdt':
+        reg.set_params(n_estimators=1000, min_samples_leaf=6)
+    elif name == 'rf':
+        reg.set_params(n_estimators=1000, min_samples_leaf=2)
+
+    reg.fit(train_data[features], train_data['cnt_log'])
+    
+    preds[name] = reg.predict(test_data[features])
+    preds[name] = np.exp(preds[name]) - 1 
+test_pred = 0.5 * preds['gbdt'] + 0.5 * preds['rf']
+test_pred[test_pred < 0] = 0 
+
+submission_df = pd.read_csv('data/sample_submission.csv')
+submission_df['cnt'] = test_pred
+submission_df.to_csv('submission/final_submission.csv', index=False)
